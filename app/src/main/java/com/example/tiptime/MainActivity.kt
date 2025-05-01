5eb722a49499eaa024b82a1dccc9f90be0a409aa
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-01 20:45:46 -0300

    Add EditNumberField

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -30,6 +31,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -54,7 +56,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = modifier,
+    )
+}
 @Composable
 fun TipTimeLayout() {
     Column(
@@ -70,6 +79,9 @@ fun TipTimeLayout() {
             modifier = Modifier
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
+        )
+        EditNumberField(
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
         )
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
