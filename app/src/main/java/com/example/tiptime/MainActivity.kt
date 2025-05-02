bbc2a22ffb37ed320d8ea9152442262f23910bd3
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-02 11:48:25 -0300

    Add a tip-percentage text field.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -63,12 +64,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun EditNumberField(value: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
+fun EditNumberField(@StringRes label: Int, value: String, onValueChanged: (String) -> Unit, modifier: Modifier = Modifier) {
     TextField(
         value = value,
-        onValueChange = onValueChange,
+        onValueChange = onValueChanged,
         singleLine = true,
-        label = {Text(stringResource(R.string.bill_amount))},
+        label = {Text(stringResource(label))},
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier,
     )
@@ -76,8 +77,10 @@ fun EditNumberField(value: String, onValueChange: (String) -> Unit, modifier: Mo
 @Composable
 fun TipTimeLayout() {
     var amountInput by remember {mutableStateOf("")};
+    var tipInput by remember { mutableStateOf("") }
     val amount = amountInput.toDoubleOrNull() ?: 0.0;
-    val tip = calculateTip(amount);
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent);
 
     Column(
         modifier = Modifier
@@ -94,9 +97,16 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         EditNumberField(
+            label = R.string.bill_amount,
             value = amountInput,
-            onValueChange = {amountInput = it},
+            onValueChanged = {amountInput = it},
             modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            value = tipInput,
+            onValueChanged = { tipInput = it},
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
         )
         Text(
             text = stringResource(R.string.tip_amount, tip),
